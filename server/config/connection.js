70b75ea0d1fe4f4f21bcb70aa47361f6245398c7
oliverLo78 <oliverberto@gmail.com>
2023-04-25 16:01:20 -0400

    deploying1

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/brain-book',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/brian-book',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
